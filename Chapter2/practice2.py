a91c6be225c35730ff07cbfd1bc8bc300a814705
tanushrithakare <tanushrithakare472@gmail.com>
2025-12-28 20:31:44 +0530

    28/12/25

--- Chapter2/practice2.py
+++ Chapter2/practice2.py
@@ -1,4 +1,28 @@
-Orignal=(input("Enter a Number:"))
-Converted=float(Orignal)
-print("Orignal No is:",Orignal,type(Orignal))
-print("Converted No is:",Converted,type(Converted))
+# Orignal=(input("Enter a Number:"))
+# Converted=float(Orignal)
+# print("Orignal No is:",Orignal,type(Orignal))
+# print("Converted No is:",Converted,type(Converted))
+
+
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the second number:"))
+# sum=num1+num2
+# print("without conversion:",sum)
+# print("with conversion:",float(sum))
+
+# list=["anshika","akash"]
+# if("anshika" not in list):
+#   print("not present")
+# else:
+#   print ("present")
+
+
+# product= 1
+# for count in range(4):
+#  product=product*(count+1)
+#  print(product)
+# 
+for i in range(1,9):
+ if i == 3:
+  continue
+print(i)
---
+++ Chapter4/str.py
@@ -0,0 +1,24 @@
+str1="akash"
+str2='''akash'''
+str3='akash'
+print("type of string 1 is :",type(str1))
+print(type(str2))
+print(type(str3))
+list1=[1,2,3]
+list2=["akash","tanu","riya",1]
+print("type of list1 is :",type(list1))
+print("type os list2:",type(list2))
+tup1=(1,2,3)
+tup=("aksh","tanu")
+print("type of tup is:",type(tup))
+print("type of tup 1 is:",type(tup1))
+tup3=(10,)
+print("type of tup3 is:",type(tup3))
+dic1={"tanu":1,"akash":1}
+print(type(dic1))
+print(dict.keys(dic1))
+capital_city={'nepal':'kathamndu','italy':'rome'}
+print(type(capital_city))
+print(dict.keys(capital_city))
+print(dict.values(capital_city))
+
